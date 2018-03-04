3f9da77069ac525193ddfff2fd78665391b3472e
An Onion <stephanie.ding@outlook.com>
2018-03-04 04:07:45 -0800

    minor date change for 2018

--- player.hpp
+++ player.hpp
@@ -6,11 +6,35 @@
 #include "board.hpp"
 using namespace std;
 
+static int boardScores[8][8] = {
+    {50, -1, 5, 2, 2, 5, -1, 50},
+    {-1, -10, 1, 1, 1, 1, -10, -1},
+    {5, 1, 1, 1, 1, 1, 1, 5},
+    {2, 1, 1, 0, 0, 1, 1, 2},
+    {2, 1, 1, 0, 0, 1, 1, 2},
+    {5, 1, 1, 1, 1, 1, 1, 5},
+    {-1, -10, 1, 1, 1, 1, -10, -1},
+    {50, -1, 5, 2, 2, 5, -1, 50}
+};
+
 class Player {
+private:
+    // TODO: Part of refimp; remove!
+    Side side;
+    Side oppSide;
+    Board board;
+    int bestMoveX;
+    int bestMoveY;
+
 
 public:
     Player(Side side);
     ~Player();
+
+    Move *doMove(Move *opponentsMove, int msLeft);
+    int miniMax(Board board, Side currSide, int depth);
+    int evaluateMove(Move *move, Board *board);
+    void setBoard(Board *board);
 
     Move *doMove(Move *opponentsMove, int msLeft);
 
