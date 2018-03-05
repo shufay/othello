18f617f55343853fc76030df2c0f6a3d7116977a
Kenyon Prater <kenyonprater3.14@gmail.com>
2018-03-04 16:15:20 -0800

    A rough draft of the minimax function

--- player.cpp
+++ player.cpp
@@ -1,5 +1,8 @@
 #include "player.hpp"
 
+#define RECURSIVE_DEPTH 2
+#define otherSide(x) (x == BLACK) ? WHITE : BLACK
+
 /*
  * Constructor for the player; initialize everything here. The side your AI is
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
@@ -8,7 +11,7 @@
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
-
+    playerside = side;
     /*
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
@@ -20,6 +23,7 @@ Player::Player(Side side) {
  * Destructor for the player.
  */
 Player::~Player() {
+
 }
 
 /*
@@ -36,9 +40,47 @@ Player::~Player() {
  * return nullptr.
  */
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
-    /*
-     * TODO: Implement how moves your AI should play here. You should first
-     * process the opponent's opponents move before calculating your own move
-     */
+    if(testingMinimax) {
+        minimax_data minmaxedmove = getMinimaxMove(board, playerside, 0);
+        Move *move = new Move(-1,-1);
+        move->setX(minmaxedmove.move.getX());
+        move->setY(minmaxedmove.move.getY());
+        return move;
+    }
     return nullptr;
+}
+
+/**
+ * @brief determines the move that will leave the selected side with the most score after a
+ *      specified recursive depth defined above.
+ * @param board a pointer to the board. May be a hypothetical future board state
+ */
+minimax_data Player::getMinimaxMove(Board *board, Side side, int depth) {
+    if(depth == RECURSIVE_DEPTH) {
+        minimax_data retval = {Move(-1,-1), board->count(side) - board->count(otherSide(side))};
+        return retval;
+    }
+    minimax_data retval = {Move(-1,-1), -65}; // An impossibly bad score
+    for(int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            Move testmove = Move(i, j);
+            if (board->checkMove(&testmove, side)) {
+                Board *next_board = board->copy();
+                next_board->doMove(&testmove, side);
+                minimax_data opponentmove = getMinimaxMove(next_board, otherSide(side), depth+1);
+                if (opponentmove.move.getX() == -1 && opponentmove.move.getY() == -1) {
+                    opponentmove.score = next_board->count(side) - next_board->count(otherSide(side));
+                    // If the opponent's move is -1,-1, then there are no valid moves. That means
+                    // the game is over and the score after this round is the final score.
+                }
+                delete next_board;
+                if (retval.score < -opponentmove.score) { // Negate; what's bad for opponent good for us
+                    retval.score = -opponentmove.score;
+                    retval.move = testmove;
+                }
+            }
+        }
+    }
+    cerr << depth << " " << retval.score << " " << retval.move.getX() << " " << retval.move.getY() << " " << endl;
+    return retval;
 }
--- player.hpp
+++ player.hpp
@@ -4,9 +4,18 @@
 #include <iostream>
 #include "common.hpp"
 #include "board.hpp"
+
+
 using namespace std;
 
+struct minimax_data {
+    Move move;
+    int score;
+};
+
 class Player {
+private:
+    minimax_data getMinimaxMove(Board *board, Side side, int depth);
 
 public:
     Player(Side side);
@@ -16,6 +25,8 @@ public:
 
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
+    Side playerside;
+    Board *board;
 };
 
 #endif
