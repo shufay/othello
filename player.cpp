096721c9d5bd7e06c555c645d9991baf31c39bb5
Ung Shu Fay <shufay.ung@gmail.com>
2018-03-06 16:26:19 -0800

    changed player name

--- player.cpp
+++ player.cpp
@@ -10,7 +10,7 @@
  */
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
-    testingMinimax = false;
+    testingMinimax = true;
     /*
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
